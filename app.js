b02cdaeea22c825cd8c4e82256b54661a2836b86
Damien White <damien.white@ocuk.local>
2023-07-18 13:24:22 +0100

    fixing pop-up location on resize

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const shareButton = document.getElementById("shareElement");
 const shareContainer = document.getElementById("shareContainer");
 
 
-shareButton.addEventListener("mouseover", displayContainer);
+shareButton.addEventListener("click", displayContainer);
 shareButton.addEventListener("mouseleave", removeContainer);
 
 function displayContainer(){
+    if (document.getElementById("shareContainer").style.display === "flex"){
+        document.getElementById("shareContainer").style.display = "none"
+    } else
         document.getElementById("shareContainer").style.display = "flex";
 }
-setTimeout(function(){
-    document.getElementById("shareContainer").style.display = "none";
-}), 1000;
+
 
