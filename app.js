cb2b8b4b3e4af075b3590f0fe60fdf1e8e2cc678
Damien White <damienkwhite@gmail.com>
2023-07-23 20:11:51 +0100

    stopped desktop share pop-up from showing on mobile screen size

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 const shareButton = document.getElementsByClassName("shareBtn")[0];
 const shareContainer = document.getElementById("shareElement");
+const profileBlock = document.getElementsByClassName("profileInfoBlock")[0];
+const mainContainer = document.getElementById("container");
 
 shareButton.addEventListener("click", displayContainer);
 
 
-function displayContainer(){
-    if (document.getElementById("shareElement").style.display === "none"){
-        document.getElementById("shareElement").style.display = "flex"
-        console.log("it works!")
-    }  else {
-        console.log("it doesn't work!")
-        document.getElementById("shareElement").style.display = "none"; 
-    }
-
-}
+function displayContainer() {
+    const computedStyles = window.getComputedStyle(mainContainer);
+    const flexDirection = computedStyles.getPropertyValue("flex-direction");
+  
+    if (flexDirection === "column") {
+      // Hide the shareContainer if mainContainer's flex-direction is "column"
+      shareContainer.style.display = "none";
+    } else if (shareContainer.style.display === "none") {
+      shareContainer.style.display = "flex";
+      console.log("it works!");
+    } else {
+        console.log("it doesn't work!");
+        shareContainer.style.display = "none";
+      }
+  }
+  
