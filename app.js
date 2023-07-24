a75583b398c132e8606d1767fbc0e858739a9001
Damien White <damienkwhite@gmail.com>
2023-07-24 22:04:33 +0100

    it's getting complicated... adding js for mobile pop-up share element

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const shareButton = document.getElementsByClassName("shareBtn")[0];
 const shareContainer = document.getElementById("shareElement");
 const profileBlock = document.getElementsByClassName("profileInfoBlock")[0];
+const profileBlockImg = document.getElementsByClassName("proImg")[0];
+const profileBlockInfo = document.getElementsByClassName("info")[0];
+const profileContainer = document.getElementById("profileContainer");
 const mainContainer = document.getElementById("container");
 
 shareButton.addEventListener("click", displayContainer);
@@ -9,10 +12,14 @@ shareButton.addEventListener("click", displayContainer);
 function displayContainer() {
     const computedStyles = window.getComputedStyle(mainContainer);
     const flexDirection = computedStyles.getPropertyValue("flex-direction");
-  
     if (flexDirection === "column") {
       // Hide the shareContainer if mainContainer's flex-direction is "column"
-      shareContainer.style.display = "none";
+      profileBlockImg.style.display = "none";
+      profileBlockInfo.style.display = "none";
+      profileContainer.style.backgroundColor = "#48556a";
+      profileBlock.style.flexDirection = "row-reverse";
+
+      console.log("turn black pls")
     } else if (shareContainer.style.display === "none") {
       shareContainer.style.display = "flex";
       console.log("it works!");
