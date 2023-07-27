185f5f24cf19f7e61bb6630081705db5ef416ad3
Damien White <damienkwhite@gmail.com>
2023-07-27 21:59:10 +0100

    shunk js a lot after research

--- app.js
+++ app.js
@@ -1,26 +1,8 @@
-/* const shareButton = document.getElementsByClassName("shareBtn")[0];
-const profileInfoBlock = document.getElementsByClassName("profileInfoBlock")[0];
-
-const shareContainer = document.getElementById("shareElement");
-const mainContainer = document.getElementById("container");
-
-shareButton.addEventListener("click", displayContainer);
-
-
-function displayContainer() {
-    const computedStyles = window.getComputedStyle(mainContainer);
-    const flexDirection = computedStyles.getPropertyValue("flex-direction");
-    if (flexDirection === "column") {
-      // Hide the shareContainer if mainContainer's flex-direction is "column"
-      profileInfoBlock.style.display = "none"
-      shareContainer.style.display = "none"
-      console.log("turn black pls")
-    } else if (shareContainer.style.display === "none") {
-      shareContainer.style.display = "flex";
-      console.log("it works!");
-    } else {
-        console.log("it doesn't work!");
-        shareContainer.style.display = "none";
-      }
+function toggleShare(){
+  const shareButton = document.getElementById("shareElement");
+  if (shareButton.style.display === "none"){
+    shareButton.style.display = "flex"
+  } else {
+    shareButton.style.display = "none"
   }
-   */
+};
